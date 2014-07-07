ce79c88b2e8ec833a190303921221af27173e891
rutkat <tom@ratetourgudies.com>
2014-07-07 13:41:26 -0700

    creating correct obj references

---
+++ patterns/obj-namespace1.js
@@ -0,0 +1,12 @@
+//If you need to create a reference to an existing method of an object, be sure to do it within that object's namespace to preserve the value of 'this'.
+var MyObject = function() {}
+
+MyObject.prototype.whoAmI = function() {
+    console.log(this === window ? "window" : "MyObj");
+};
+
+var obj = new MyObject();
+obj.w = obj.whoAmI;   // still in the obj namespace
+
+obj.whoAmI();  // outputs "MyObj" (as expected)
+obj.w();       // outputs "MyObj" (as expected)
