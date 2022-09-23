56755a19a132ea73f5fa026a02dd7cc780afb350
Rutkat <tom@ratetourguides.com>
2022-09-23 12:29:47 -0500

    Update day-of-year.js

--- day-of-year.js
+++ day-of-year.js
@@ -5,3 +5,5 @@ dayOfYear(new Date());
 // Find number of days between two dates
 const dateDiff = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000);
 
+// Check if date is valid
+const isDateValid = (...val) => !Number.isNaN(new Date(...val).valueOf());
