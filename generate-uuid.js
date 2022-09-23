fb047563eb852323136565a26e63c742f589d0fb
Rutkat <tom@ratetourguides.com>
2022-09-23 12:21:48 -0500

    Create generate-uuid.js

---
+++ generate-uuid.js
@@ -0,0 +1,4 @@
+// Generate a UUID (without using uuidv4 npm package)
+const generateRandomUUID = a => (a ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, generateRandomUUID));
+
+generateRandomUUID();
